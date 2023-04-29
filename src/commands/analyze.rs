54479beddc95f08fd8bfc4e9cee6b32c23362392
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2023-04-29 13:01:01 +0200

    wip

--- src/commands/analyze.rs
+++ src/commands/analyze.rs
@@ -1,7 +1,8 @@
+use crate::utils::tmutil::{compare, list_backups, CompareArgs};
 use anyhow::Result;
 use indicatif::{MultiProgress, ProgressBar, ProgressState, ProgressStyle};
 use log::info;
-use std::{fmt::Write, process::Command, time::Duration};
+use std::{fmt::Write, time::Duration};
 
 pub fn run() -> Result<()> {
     info!("Starting analyze process");
@@ -19,47 +20,31 @@ pub fn run() -> Result<()> {
     pb.set_message("Getting list of backups");
     pb.enable_steady_tick(Duration::from_millis(100));
 
-    let backups = Command::new("tmutil")
-        .arg("listbackups")
-        .output()
-        .expect("Failed to execute tmutil");
+    let backup_paths = list_backups()?;
 
     pb.finish();
 
-    let backups = String::from_utf8(backups.stdout)?;
-
-    let backup_paths = backups
-        .split("\n")
-        .filter(|p| !p.trim().is_empty())
-        .collect::<Vec<&str>>();
-
     let mut commands = vec![];
 
-    // Compare current state (tmutil compare -aX)
-    commands.push(vec![]);
+    // // Compare current state (tmutil compare -aX)
+    // commands.push(CompareArgs::Current);
 
     // Compare past backups
     for (compare_path, compare_to_path) in backup_paths[1..].iter().zip(backup_paths.iter()) {
-        commands.push(vec![compare_path, compare_to_path])
+        commands.push(CompareArgs::Backups {
+            first: compare_path.to_string(),
+            second: compare_to_path.to_string(),
+        })
     }
 
     for args in commands {
         let pb = m.add(ProgressBar::new(0));
 
-        let mut final_args = vec!["compare", "-aX"];
-        final_args.extend(args);
-
         pb.set_style(sty.clone());
-        pb.set_message(format!(
-            "Analyzing backup (tmutil {:?})",
-            final_args.join(" ")
-        ));
+        pb.set_message(format!("Analyzing backup (tmutil {:?})", &args));
         pb.enable_steady_tick(Duration::from_millis(100));
 
-        let backups = Command::new("tmutil")
-            .args(&final_args)
-            .output()
-            .expect("Failed to execute tmutil");
+        compare(&args)?;
 
         pb.finish();
     }
--- src/utils/tmutil.rs
+++ src/utils/tmutil.rs
@@ -27,3 +27,49 @@ pub fn remove_exclusion(path: &str) -> Result<()> {
 
     Ok(())
 }
+
+#[derive(Debug)]
+pub enum CompareArgs {
+    Current,
+    Backups { first: String, second: String },
+}
+
+pub fn compare(args: &CompareArgs) -> Result<()> {
+    let mut cmd = &mut Command::new("tmutil");
+    cmd = cmd.arg("compare").arg("-aX");
+
+    match args {
+        CompareArgs::Current => {}
+        CompareArgs::Backups { first, second } => cmd = cmd.args([first, second]),
+    }
+
+    let output = cmd.output()?;
+
+    if !output.status.success() {
+        debug!("{:?}", String::from_utf8(output.stderr)?);
+        return Err(anyhow!("Failed to execute compare"));
+    }
+
+    assert!(output.status.success());
+
+    dbg!(output.stdout);
+
+    Ok(())
+}
+
+pub fn list_backups() -> Result<Vec<String>> {
+    let backups = Command::new("tmutil")
+        .arg("listbackups")
+        .output()
+        .expect("Failed to execute tmutil");
+
+    let backups = String::from_utf8(backups.stdout)?;
+
+    let backup_paths = backups
+        .split("\n")
+        .filter(|p| !p.trim().is_empty())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    Ok(backup_paths)
+}
